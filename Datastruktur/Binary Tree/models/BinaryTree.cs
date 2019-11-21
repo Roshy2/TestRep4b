5a3bd1727d87139ce2fec96c64312119b065a7ba
Roshy2 <roshy@live.at>
2019-11-21 13:48:30 +0100

    Mergenkonflikt - Versuch

--- Datastruktur/Binary Tree/models/BinaryTree.cs
+++ Datastruktur/Binary Tree/models/BinaryTree.cs
@@ -17,6 +17,7 @@ namespace Binary_Tree.models
         {
             this._root = new BinaryTreeItem(Item, null, null, 1);
         }
+        public int tiktok;
 
         //Methode Add
         public bool Add(int itemToAdd)
