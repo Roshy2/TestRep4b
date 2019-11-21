ed4863f13dfb01122b291f9f05ff233ac7d4b779
Roshy2 <43375611+Roshy2@users.noreply.github.com>
2019-11-21 13:29:09 +0100

    Update BinaryTree.cs

--- Datastruktur/Binary Tree/models/BinaryTree.cs
+++ Datastruktur/Binary Tree/models/BinaryTree.cs
@@ -18,7 +18,7 @@ namespace Binary_Tree.models
             this._root = new BinaryTreeItem(Item, null, null, 1);
         }
 
-        pubic int tutut;
+        public int tutut;
         //Methode Add
         public bool Add(int itemToAdd)
         {
